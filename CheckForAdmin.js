af5b320aa91bc9d62b9d58ccb4c47f220ddcef05
Hassan-al-sayegh <haal19ag@student.cbs.dk>
2020-05-14 00:04:47 +0200

    viderfølger af CheckForAdmin kodning

--- CheckForAdmin.js
+++ CheckForAdmin.js
@@ -13,3 +13,27 @@ const columnAndTypes = [{
 }];
 
 
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return Promise.all(
+            columnAndTypes.map(c => {
+                return queryInterface.addColumn(
+                    'Users',
+                    c.name,
+                    c.type(Sequelize)
+                )
+            })
+        );
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return Promise.all(
+            columnAndTypes.map(c => {
+                return queryInterface.removeColumn(
+                    'Users',
+                    c.name,
+                )
+            })
+        )
+    }
+};
