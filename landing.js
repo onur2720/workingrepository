bd70935a9b2a6c5c3c66fffe63b2f433e0512221
tayyablatif <mola19af@student.cbs.dk>
2020-05-14 00:03:33 +0200

    landing.js

--- landing.js
+++ landing.js
@@ -13,6 +13,22 @@ include ./common/navbar.pug
 h1.mb-3 Her kan du udfylde formularen til dit lån
 p.lead
 p.lead.mb-4
+form(action="/", method="POST")
+    .input-group.mb-3
+input(name="loan_email", type="email", placeholder="Email").form-control
+    .input-group-append
+input(name="loan_number", type="number", placeholder="cpr-nr").form-control
+    .input-group-append
+input(name="loan_text", type="text", placeholder="Navn").form-control
+    .input-group-append
+input(name="loan_number1", type="number1", placeholder="Beløb").form-control
+    .input-group-append
+input(name="loan_number2", type="number2", placeholder="Løbetid").form-control
+    .input-group-append
+
+button(type="submit").btn.btn-primary Ansøg
+include ./common/footer.pug
++footer()
 
 
 
